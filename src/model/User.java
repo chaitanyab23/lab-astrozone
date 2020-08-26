625973dd81e6fdca3f9f7105ccbe229a512b49f0
chaitanyab23 <chaitanyabhagat23@gmail.com>
2020-08-26 17:42:02 +0530

    chaitanya

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,47 @@
 package model;
 
 //Type your code here
+public class User{
+	public String name;
+	public String email;
+	public String dob;
+	public String gender;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	public User(String name, String email, String dob, String gender) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.gender = gender;
+	}
+	
+	
+	
+	
+}
--- src/service/AstroCalculator.java
+++ src/service/AstroCalculator.java
@@ -1,3 +1,89 @@
 package service;
 
+import model.User;
+
 // Type your code
+public class AstroCalculator{
+	
+	public String findSign(User u1) {
+		String astrosign="";
+		String date[]= u1.getDob().split("-");
+		int day =Integer.parseInt(date[0]);
+		int month=Integer.parseInt(date[1]);
+
+		
+		if (month == 1) {
+		    if (day < 20)
+		       astrosign = "Capricorn";
+		    else
+		       astrosign = "Aquarius";
+		 }
+		 else if (month == 2) {
+		    if (day < 19)
+		       astrosign = "Aquarius";
+		    else
+		       astrosign = "Pisces";
+		 }
+		 else if(month == 3) {
+		    if (day < 21)
+		       astrosign = "Pisces";
+		    else
+		       astrosign = "Aries";
+		 }
+		 else if (month == 4) {
+		    if (day < 20)
+		       astrosign = "Aries";
+		    else
+		       astrosign = "Taurus";
+		 }
+		 else if (month == 5) {
+		    if (day < 21)
+		       astrosign = "Taurus";
+		    else
+		       astrosign = "Gemini";
+		 }
+		 else if( month == 6) {
+		    if (day < 21)
+		       astrosign = "Gemini";
+		    else
+		       astrosign = "Cancer";
+		 }
+		 else if (month == 7) {
+		    if (day < 23)
+		       astrosign = "Cancer";
+		    else
+		       astrosign = "Leo";
+		 }
+		 else if( month == 8) {
+		    if (day < 23)
+		       astrosign = "Leo";
+		    else
+		       astrosign = "Virgo";
+		 }
+		 else if (month == 9) {
+		    if (day < 23)
+		       astrosign = "Virgo";
+		    else
+		       astrosign = "Libra";
+		 }
+		 else if (month == 10) {
+		    if (day < 23)
+		       astrosign = "Libra";
+		    else
+		       astrosign = "Scorpio";
+		 }
+		 else if (month == 11) {
+		    if (day < 22)
+		       astrosign = "scorpio";
+		    else
+		       astrosign = "Sagittarius";
+		 }
+		 else if (month == 12) {
+		    if (day < 22)
+		       astrosign = "Sagittarius";
+		    else
+		       astrosign ="Capricorn";
+		 }
+		return astrosign;
+	}
+}
